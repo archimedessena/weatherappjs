42d7c7f1c3da6fae69a8a28e3ea8828f70d43f5c
Archimedes <archimedessena@gmail.com>
2020-10-18 21:31:45 +0000

    js added

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const api = {
-    key:
-    base:
+    key: "d50e8c0dec3297019f622b42d0482871",
+    base: "https://api.openweathermap.org/data/2.5/"
+ 
+}
 
-};
-
-const searcbox = document.queryselector('.search-box');
+const searchbox = document.querySelector('.search-box');
 searchbox.addEventListener('keypress', setQuery);
 
 function setQuery(evt) {
-    if (evt.keycode == 13) {
+    if (evt.keyCode == 13) {
         getResults(searchbox.value);
-        
+        console.log(searchbox.value);
     }
 }
 
@@ -22,7 +22,34 @@ function getResults (query) {
 
 
 function displaysResults (weather) {
-    console.log(weather){
-        console.log(weather);
-    }
+    console.log(weather);
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    let date = Document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>˚c</span>`;
+
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
+
+    let hilow = document.querySelector('.hi-low');
+    hilow.innertext = `${Math.round(weather.main.temp_min)}˚c / ${Math.round(weather.main.temp_max)}˚c`;
+
+} 
+
+function dateBuilder (d) {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
 }
